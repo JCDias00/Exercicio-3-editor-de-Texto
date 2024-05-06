fa1044912db4db9f6f031b320a1775d60520d8b6
Julio Cesar <jccardosodias0@gmail.com>
2024-05-05 22:18:47 -0300

    finalizando o exercicio

--- obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619fe700c210a4ec485d2aa0d88d7c0c9498f990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fd0e23c3b05da6c4035a642d47953d3c1231888")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
