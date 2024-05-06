2fd0e23c3b05da6c4035a642d47953d3c1231888
Julio Cesar <jccardosodias0@gmail.com>
2024-05-05 22:10:04 -0300

    salvando o arquivo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.IO;
+
+
 Menu();
 
 static void Menu()
@@ -10,38 +12,54 @@ static void Menu()
     Console.WriteLine("2 - Criar novo arquivo");
     Console.WriteLine("0 - Sair");
 
-    short option  = short.Parse(Console.ReadLine());
+    short option = short.Parse(Console.ReadLine());
 
-    switch(option)
+    switch (option)
     {
-        case 0: System.Environment.Exit(0); break; 
+        case 0: Environment.Exit(0); break;
         case 1: Abrir(); break;
         case 2: Editar(); break;
         default: Menu(); break;
 
-    
+
 
     }
 }
-static void Abrir(){}
+static void Abrir() { }
 
 static void Editar()
+
 {
     Console.Clear();
     Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
     Console.WriteLine("----------------");
 
-    string text  = "";
+    string text = "";
 
     do
     {
         text += Console.ReadLine();
-        text += Environment.NewLine();
+        text += Environment.NewLine;
     }
 
-    while(Console.ReadKey().Key != ConsoleKey.Escape);
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
 
-    Console.Write(text);
+    Salvar(text);
+}
 
+static void Salvar(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+    {
+        file.Write(text);
+    }
+
+    Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+    Console.ReadLine();
+    Menu();
 
 }
--- obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619fe700c210a4ec485d2aa0d88d7c0c9498f990")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
